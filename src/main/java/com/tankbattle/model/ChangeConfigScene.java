e24a7c21651d463db990711d4a0a71b1efd6be44
ZZJ <2111976953@qq.com>
2025-06-05 15:39:54 +0800

    加入玩家子弹冷却时间修改

--- src/main/java/com/tankbattle/model/ChangeConfigScene.java
+++ src/main/java/com/tankbattle/model/ChangeConfigScene.java
@@ -34,25 +34,39 @@ public class ChangeConfigScene {
         //Speed
         Text textSpeedGamer =new Text("玩家速度：");
         Slider sliderSpeedGamer = new Slider(0, 20, 10);
-        sliderSpeedGamer.setShowTickLabels(true);   // 显示数字标签（如50）
-        sliderSpeedGamer.setShowTickMarks(true);   // 显示刻度线
-        sliderSpeedGamer.setMajorTickUnit(1);     // 每10一格
-        sliderSpeedGamer.setMinorTickCount(4);     // 每个主刻度之间有4个小刻度
-        sliderSpeedGamer.setBlockIncrement(1);     // 拖动时步进为 1
+        sliderSpeedGamer.setShowTickLabels(true);
+        sliderSpeedGamer.setShowTickMarks(true);
+        sliderSpeedGamer.setMinorTickCount(4);
+        sliderSpeedGamer.setBlockIncrement(1);
         GameConfig.GAMER_SPEED.bindBidirectional(sliderSpeedGamer.valueProperty());
 
         Text textSpeedE=new Text("玩家速度：");
         Slider sliderSpeedE = new Slider(0, 20, 1);
-        sliderSpeedE.setShowTickLabels(true);   // 显示数字标签（如50）
-        sliderSpeedE.setShowTickMarks(true);   // 显示刻度线
-        sliderSpeedE.setMajorTickUnit(1);     // 每10一格
-        sliderSpeedE.setMinorTickCount(4);     // 每个主刻度之间有4个小刻度
-        sliderSpeedE.setBlockIncrement(1);     // 拖动时步进为 1
+        sliderSpeedE.setShowTickLabels(true);
+        sliderSpeedE.setShowTickMarks(true);
+        sliderSpeedE.setMajorTickUnit(1);
+        sliderSpeedE.setMinorTickCount(4);
+        sliderSpeedE.setBlockIncrement(1);
         GameConfig.ENEMY_SPEED.bindBidirectional(sliderSpeedE.valueProperty());
 
+        //bullet cooldown
+
+        Text textBulletCoolGamer = new Text("玩家子弹冷却时间（ms）：");
+        Slider sliderBulletCoolGamer = new Slider(100, 2000, 1000); // 100ms 到 2000ms（即0.1s到2s）
+        sliderBulletCoolGamer.setShowTickLabels(true);
+        sliderBulletCoolGamer.setShowTickMarks(true);
+        sliderBulletCoolGamer.setMajorTickUnit(500); // 每500ms一格
+        sliderBulletCoolGamer.setMinorTickCount(4);
+        sliderBulletCoolGamer.setBlockIncrement(50);
+
+        sliderBulletCoolGamer.valueProperty().addListener((observable, oldValue, newValue) -> {
+            GameConfig.GAMER_BULLET_COOLDOWN = newValue.intValue() * 1_000_000; // 转换为纳秒
+            System.out.println("NEW_COOLDOWN: " + GameConfig.GAMER_BULLET_COOLDOWN);
+        });
 
 
-        VBox root=new VBox(btn1,textHP,sliderHP, textSpeedGamer, sliderSpeedGamer,textSpeedE,sliderSpeedE);
+
+        VBox root=new VBox(btn1,textHP,sliderHP, textSpeedGamer, sliderSpeedGamer,textSpeedE,sliderSpeedE,textBulletCoolGamer,sliderBulletCoolGamer);
         scene = new Scene(root, Main.sceneWid, Main.sceneHei);
     }
     public Scene getScene() {
